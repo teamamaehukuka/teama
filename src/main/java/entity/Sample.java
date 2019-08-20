365e094a3841881b8b083a80a364e078fabac673
SLC <SLC@DESKTOP-KC9V5EH>
2019-08-20 11:15:16 +0900

    Lombok導入

---
+++ src/main/java/entity/Sample.java
@@ -0,0 +1,15 @@
+package entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Sample {
+
+	@Setter @Getter
+	int idl;
+	@Setter @Getter
+	String user;
+	@Setter @Getter
+	String col;
+
+}
